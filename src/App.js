8d0c11110c056df28bccab38c4086f9209192952
Michael Desanker <mdesanker@gmail.com>
2021-11-14 10:59:18 -0500

    fix: correct useEffect import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
 import Form from "./components/form/Form";
 import Resume from "./components/resume/Resume";
 import uniqid from "uniqid";
