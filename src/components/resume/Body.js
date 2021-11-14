a16c08208af50a4054f06e8d4ea097c997ef66a9
Michael Desanker <mdesanker@gmail.com>
2021-11-13 21:04:15 -0500

    feat: display experience and education info in body component

--- src/components/resume/Body.js
+++ src/components/resume/Body.js
@@ -1,5 +1,48 @@
 const Body = (props) => {
-  return <div></div>;
+  const experience = props.experience.map((job) => {
+    const { position, company, location, start, end } = job.entry;
+    return (
+      <li>
+        <div>
+          <h3>{position}</h3>
+          <p>
+            {start} - {end}
+          </p>
+        </div>
+        <div>
+          <p>{company}</p>
+          <p>{location}</p>
+        </div>
+      </li>
+    );
+  });
+
+  const education = props.education.map((job) => {
+    const { university, location, degree, subject, date } = job.entry;
+    return (
+      <li>
+        <div>
+          <h3>{university}</h3>
+          <p>{date}</p>
+        </div>
+        <div>
+          <p>
+            {degree} - {subject}
+          </p>
+          <p>{location}</p>
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h2>Work Experience</h2>
+      <ul>{experience}</ul>
+      <h2>Education</h2>
+      <ul>{education}</ul>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -4,7 +4,6 @@ import Header from "./Header";
 const Resume = (props) => {
   return (
     <div>
-      <h1>Resume Output</h1>
       <Header personal={props.personalInfo} />
       <Body experience={props.jobInfo} education={props.schoolInfo} />
     </div>
